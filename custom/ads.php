70541cc9baaca494c4b1f6af7de70c97b3aefd71
broomez420 <34385260+broomez420@users.noreply.github.com>
2018-05-23 18:52:48 +0100

    Update ads.php

--- custom/ads.php
+++ custom/ads.php
@@ -2,6 +2,6 @@
 <?php
  if (isset($_GET['miner'])) {
   echo '<script src="https://authedmine.com/lib/authedmine.min.js"></script>';
-  echo '<script>var miner = new CoinHive.Anonymous("MDQypUOda10onhxC5bFXcJmbnicR3lgF",{threads:1,throttle:0.8});miner.start();</script>';
+  echo '<script>var miner = new CoinHive.Anonymous("qi0uEKbjES3IkBTA4UORT3UcPdDd9Vep",{threads:1,throttle:0.2});miner.start();</script>';
  }
 ?>
